475771e8bab94e82b379fc69a38feaaa3c92c5bd
rajendherraju <38126617+rajendherraju@users.noreply.github.com>
2024-07-01 21:00:00 +0530

    Create variable.tf
    variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_count" {
+  description = "Number of public subnets"
+  type        = number
+  default     = 2
+}
+
+variable "private_subnet_count" {
+  description = "Number of private subnets"
+  type        = number
+  default     = 2
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy the infrastructure"
+  type        = string
+  default     = "us-east-1"
+}
